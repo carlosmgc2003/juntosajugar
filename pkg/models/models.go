a9d08ef0121763e4b3d6010ef906b81d7fb5600f
carlosmgc2003 <carlosmgc2003@gmail.com>
2020-08-28 20:53:41 -0300

    Refactorizado los modelos para satisfacer las convenciones de golang y eliminado el commonmodelfield reemplazado por gorm.Model para facilitar la implementacion del modelo en la BD

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -11,38 +11,29 @@ var (
 	ErrDuplicate = errors.New("models: duplicate insertion in DB")
 )
 
-// struct que engloba a todos los campos comunes de las demas. Los atributos escapados
-// son para la interpretacion de librerias, en este caso gorm y json
-type commonModelFields struct {
-	ID        uint      `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
-}
-
 // Usuario de juntos a jugar
 type User struct {
-	gorm.Model         //ya tiene el id
-	Name        string `gorm:"unique;size:100" json:"name"`
-	Email       string `gorm:"unique;not null;size:100" json:"email"`
-	Display_pic string `gorm:"size:100" json:"display_pic_route"`
+	gorm.Model        //ya tiene el id
+	Name       string `gorm:"unique;size:100" json:"name"`
+	Email      string `gorm:"unique;not null;size:100" json:"email"`
+	DisplayPic string `gorm:"size:100" json:"display_pic_route"`
 }
 
 // Juegos de mesa
 type Boardgame struct {
-	commonModelFields        //ya tiene el id
-	Name              string `gorm:"unique;not null;size:100" json:"name"`
-	Class             string `gorm:"not null;size:40" json:"class"`
-	Display_pic       string `gorm:"unique;size:100" json:"display_pic_route"`
+	gorm.Model        //ya tiene el id
+	Name       string `gorm:"unique;not null;size:100" json:"name"`
+	Class      string `gorm:"not null;size:40" json:"class"`
+	DisplayPic string `gorm:"unique;size:100" json:"display_pic_route"`
 }
 
 // Reuniones para jugar
 type GameMeeting struct {
-	commonModelFields           //ya tiene el id
-	Place             string    `gorm:"unique;not null;size:100" json:"name"`
-	scheduled         time.Time `json:"scheduled"`
-	Owner             User      `gorm:"foreignkey:ID;not null" json:"owner_id"`
-	Players           []User    `gorm:"foreignkey:ID;not null" json:"players"`
-	Game              Boardgame `gorm:"foreignkey:ID;not null" json:"game"`
-	Max_Players       int       `gorm:"not null" json:"max_players"`
+	gorm.Model           //ya tiene el id
+	Place      string    `gorm:"unique;not null;size:100" json:"name"`
+	Scheduled  time.Time `json:"scheduled"`
+	Owner      User      `gorm:"foreignkey:ID;not null" json:"owner_id"`
+	Players    []User    `gorm:"foreignkey:ID;not null" json:"players"`
+	Game       Boardgame `gorm:"foreignkey:ID;not null" json:"game"`
+	MaxPlayers int       `gorm:"not null" json:"max_players"`
 }
