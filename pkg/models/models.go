b0ecab04ad2a556bb1ed5d4dcf874f8357b4b665
Carlos Maceira <carlosmgc2003@gmail.com>
2020-12-08 21:41:50 -0300

    Agregado el vector de Boardgames para el User

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,24 +5,27 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
+	// Extiende a gorm para que use mysql
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var (
+	//ErrDuplicate es el Error de duplicado en la BD
 	ErrDuplicate = errors.New("models: duplicate insertion in DB")
 )
 
-// Usuario de juntos a jugar
+// User es el struct de Usuario de juntos a jugar
 type User struct {
-	gorm.Model            //ya tiene el id
-	Name           string `gorm:"unique;size:100" json:"name"`
-	Email          string `gorm:"unique;not null;size:100" json:"email"`
-	DisplayPic     string `gorm:"size:100" json:"display_pic_route"`
-	IdToken        string `gorm:"size:1500" json:"tokenId"`
-	HashedPassword string `gorm:"size:60" json:"password"`
+	gorm.Model                 //ya tiene el id
+	Name           string      `gorm:"unique;size:100" json:"name"`
+	Email          string      `gorm:"unique;not null;size:100" json:"email"`
+	DisplayPic     string      `gorm:"size:100" json:"display_pic_route"`
+	IDtoken        string      `gorm:"size:1500" json:"tokenId"`
+	HashedPassword string      `gorm:"size:60" json:"password"`
+	UserBoardgames []Boardgame `gorm:"many2many:user_boardgames;"`
 }
 
-// Juegos de mesa
+// Boardgame es el Juego de mesa
 type Boardgame struct {
 	gorm.Model        //ya tiene el id
 	Name       string `gorm:"unique;not null;size:100" json:"name"`
@@ -30,14 +33,14 @@ type Boardgame struct {
 	DisplayPic string `gorm:"unique;size:100" json:"display_pic_route"`
 }
 
-// Reuniones para jugar
+// Gamemeeting Reuniones para jugar
 type Gamemeeting struct {
 	gorm.Model            //ya tiene el id
 	Place       string    `gorm:"unique;not null;size:100" json:"place"`
 	Scheduled   time.Time `gorm:"not null" json:"scheduled"`
-	OwnerId     uint
+	OwnerID     uint
 	Owner       User `gorm:"foreignKey:OwnerId"`
-	BoardgameId uint
+	BoardgameID uint
 	Boardgame   Boardgame `gorm:"foreignKey:BoardgameId"`
 	Players     []User    `gorm:"many2many:user_gamemeeting;"`
 	MaxPlayers  uint      `gorm:"not null;" json:"max_players"`
